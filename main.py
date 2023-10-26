e814036da6f1962b61bf388e5e62e55343e0cac9
GiadaGiuliaPaola <giadatamassia@gmail.com>
2023-10-26 15:03:17 +0200

    add app

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return "I did a thing!"
+def home():
+    return render_template('index.html')
 
-@app.route('/cow')
-def cow():
-  return "MooOOooO"
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
